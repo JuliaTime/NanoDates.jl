e5f5690b37211d2978e698076ad1dc9b12d742b2
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-21 02:37:09 -0400

    test file orginization
    test dir

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,36 @@
 using NanoDates, Test
 
-@testset "construction" begin
 
+@testset "nanodate" begin
+    include("nanodate.jl`")
 end
 
 @testset "accessors" begin
-
+    include("accessors.jl")
 end
 
-@testset "string" begin
+@testset "convert" begin
+    include("convert.jl")
+end
 
+@testset "compare" begin
+    include("compare.jl")
+end
+
+@testset "interop" begin
+    include("interop.jl")
+end
+
+@testset "arith" begin
+    include("arith.jl")
+end
+
+@testset "strings" begin
+    include("strings.jl")
 end
 
 @testset "accuracy" begin
-
+    include("accuracy.jl")
 end
 
 

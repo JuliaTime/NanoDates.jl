2f9a8b0a3baf85bdf31c2df448a1f681a4bece2f
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-21 13:31:24 -0400

    rename conversions.jl

--- src/conversions.jl
+++ src/conversions.jl
@@ -18,3 +18,26 @@ Dates.DateTime(x::NanoDate) = convert(Time, x)
 Dates.Time(x::NanoDate) = convert(Time, x)
 Dates.Date(x::NanoDate) = convert(Date, x)
 
+#### External Conversions
+
+nanodate2rata(nd::NanoDate) = datetime2rata(nd.datetime)
+
+rata2nanodate(rata::Integer) = NanoDate(rata2datetime(rata))
+
+function nanodate2unix(nd::NanoDate)
+    millis = (value(nd) - Dates.UNIXEPOCH)
+    nanos  = (millis * NanosecondsPerMillisecond) + nd.nanosecs
+    nanos / NanosecondsPerMillisecond
+end
+
+function unix2nanodate(x)
+    nanos = x * NanosecondsPerMillisecond
+    millis, nanos = fldmod(nanos, NanosecondsPerMicrosecond)
+    NanoDate(DateTime(Dates.UTM(millis)), nanos)
+end
+
+
+julian2nanodate(x) = NanoDate(julian2datetime(x))
+
+nanodate2julian(nd::NanoDate) = datetime2julian(DateTime(nd))
+

c9e23a2b2190e2ed0667184744ee9e21d18fe2c4
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-21 04:23:38 -0400

    rev

---
+++ docs/make.jl
@@ -0,0 +1,28 @@
+
+using Documenter
+
+makedocs(
+    # modules = [NanosecondDates],
+    sitename = "NanosecondDates",
+    authors = "Jeffrey Sarnoff",
+    clean = false,
+    strict = !("strict=false" in ARGS),
+    doctest = ("doctest=only" in ARGS) ? :only : true,
+    format = Documenter.HTML(
+        # Use clean URLs, unless built as a "local" build
+        prettyurls = !("local" in ARGS),
+        highlights = ["yaml"],
+        ansicolor = true,
+    ),
+    pages = Any[
+        "Home" => "index.md"
+    ],
+)
+
+#=
+Deploy docs to Github pages.
+=#
+Documenter.deploydocs(
+    repo="github.com/JeffreySarnoff/NanosecondDates.jl.git",
+    devbranch = "main",
+)

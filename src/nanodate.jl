d776fc446011ab0489773e91d50b39bf917de562
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-23 17:25:10 -0400

    keep

--- src/nanodate.jl
+++ src/nanodate.jl
@@ -1,22 +1,16 @@
-const Construct = Val{:NanoDate}
-const Constructor = Construct()
-
 struct NanoDate
     datetime::DateTime      # DateTime(UTM(microsec))
     nanosecs::Nanosecond    # Nanosecond(microsecond * 1_000 + nanosecond)
 
-    NanoDate(::Construct, datetime::DateTime, nanosecs::Nanosecond) =
-        new(datetime, nanosecs)
-end
+    function NanoDate(datetime::DateTime, nanosecs::Nanosecond)
+        nanos = value(nanosecs)
+        0 <= nanos < 1_000_000 && return new(datetime, nanosecs)
 
-function NanoDate(datetime::DateTime, nanosecs::Nanosecond)
-    nanos = value(nanosecs)
-    0 <= nanos < 1_000_000 && return NanoDate(Constructor, datetime, nanosecs)
-
-    millisecs = value(datetime)
-    millis, nanos = divrem(nanos, NanosecondsPerMillisecond)
-    datetime = DateTime(Dates.UTM(millisecs + millis))
-    NanoDate(Constructor, datetime, Nanosecond(nanosecs))
+        millisecs = value(datetime)
+        millis, nanos = divrem(nanos, NanosecondsPerMillisecond)
+        datetime = DateTime(Dates.UTM(millisecs + millis))
+        new(datetime, Nanosecond(nanosecs))
+    end
 end
 
 NanoDate(x::NanoDate) = x

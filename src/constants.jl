3d75e9987c0c60486b6470b7ca4c72bf9084ad18
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-23 16:19:28 -0400

    fiz

--- src/constants.jl
+++ src/constants.jl
@@ -65,5 +65,5 @@ const Epoch2000Date = Date(2000,1,1)
 const Epoch2000DateTime = DateTime(2000,1,1, 0,0,0, 0)
 const Epoch2000RataDie = Dates.value(Epoch2000Date)
 const Epoch2000Milliseconds = Dates.value(Epoch2000DateTime)
-const Epoch2000Nanoseconds  = Int128(Epoch2000Milliseconds) * NanosecondsPerMilliseconds
+const Epoch2000Nanoseconds  = Int128(Epoch2000Milliseconds) * NanosecondsPerMillisecond
 

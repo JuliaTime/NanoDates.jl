6aef196277a0d5c3f517604c56aeeb75f9156e72
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-23 23:13:26 -0400

    using UTM, UTD

--- src/NanoDates.jl
+++ src/NanoDates.jl
@@ -5,7 +5,7 @@ export nanodate2rata, rata2nanodate, nanodate2unix, unix2nanodate
 
 using Dates
 using Dates: AbstractDateTime, CompoundPeriod,
-                value, days, toms, tons
+                value, days, toms, tons, UTM, UTD
 
 include("constants.jl")
 include("nanodate.jl")

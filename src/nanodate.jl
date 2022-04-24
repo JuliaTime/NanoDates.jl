66ba7afc1e3022b45c4a70e8e70f9c3be5ec2543
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-23 23:08:16 -0400

    constructors

--- src/nanodate.jl
+++ src/nanodate.jl
@@ -26,6 +26,32 @@ NanoDate(dt::Date, us::Microsecond, ns::Nanosecond) = NanoDate(DateTime(dt), us,
 NanoDate(d::Date, us::Microsecond) = NanoDate(DateTime(dt), us)
 NanoDate(d::Date, ns::Nanosecond) = NanoDate(DateTime(d), ns)
 
+NanoDate(yr::Year, mn::Month=Month(1), dy::Day=Day(1)) = NanoDate(Date(yr, mn, dy))
+NanoDate(yr::Year, mn::Month, dy::Day, hr::Hour, mi::Minute=Minute(0), sc::Second=Second(0),
+    ms::Millisecond=Millisecond(0), us::Microsecond=Microsecond(0), ns::Nanosecond=Nanosecond(0)) =
+    NanoDate(DateTime(yr, mn, dy, hr, mi, sc, ms), us, ns)
+
+NanoDate(yr, mn=1, dy=1)) = NanoDate(Date(yr, mn, dy))
+NanoDate(yr, mn, dy, hr, mi=0, sc=0, ms=0, us=0, ns=0)) = 
+    NanoDate(DateTime(yr, mn, dy, hr, mi, sc, ms), Nanosecond(value(us) * 1_000 + value(ns)))
+
 # for internal use
-NanoDate(dt::DateTime, ns::Int) = NanoDate(dt, Nanosecond(ns))
-NanoDate(dt::Date, ns::Int) = NanoDate(dt, Nanosecond(ns))
+
+usns(ns) = ns
+usns(us, ns) = us * 1_000 + ns
+
+nanosecs(ns) = Nanosecond(ns)
+nanosecs(ns::Nanosecond) = ns
+nanosecs(us::Microsecond) = Nanosecond(1_000 * value(us))
+nanosecs(us, ns) = Nanosecond(usns(us, ns))
+nanosecs(us::Microsecond, ns::Nanosecond) = nanosecs(value(us), value(ns))
+
+NanoDate(dt::DateTime, x) = NanoDate(dt, nanosecs(x))
+NanoDate(dt::DateTime, us, ns) = NanoDate(dt, nanosecs(us, ns))
+NanoDate(d::Date, x) = NanoDate(d, nanosecs(x))
+NanoDate(d::Date, us, ns) = NanoDate(d, nanosecs(us, ns))
+
+NanoDate(; millis=0, us=0, ns=0) = NanoDate(DateTime(UTM(millis)), nanosecs(us, ns))
+
+const DateOrDateTime = value(DateTime(1,1,1))
+datedatetime(x) = (abs(x) < DateOrDateTime) ? Date(UTD(x)) : DateTime(UTM(x))

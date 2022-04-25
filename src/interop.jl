0af884c3a85c0fdbd005c690c6af2b01ff58f524
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-25 03:24:40 -0400

    promote

--- src/interop.jl
+++ src/interop.jl
@@ -1,17 +1,3 @@
-"""
-    nanonow() -> NanoDate
+Base.promote_rule(::Type{NanoDate}, ::Type{DateTIme}) = NanoDate
+Base.promote_rule(::Type{NanoDate}, ::Type{Date}) = NanoDate
 
-The user's time in the system timezone locale
-
-    nanonow(::Type{UTC}) -> NanoDate
-
-The user's time as UTC/GMT
-""" nanonow
-
-@inline function nanonow()
-    NanoDate(now())
-end
-
-@inline function nanonow(::Type{UTC})
-    NanoDate(now(UTC))
-end

99b302e05cbb5f3dfaa1123fcbed5e055906518d
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-25 07:16:55 -0400

    arithmetic

--- src/arith.jl
+++ src/arith.jl
@@ -29,4 +29,52 @@ function Base.:(-)(nd::NanoDate, x::Microsecond)
     NanoDate(nd.datetime + Millisecond(millis), Nanosecond(nanos))
 end
 
+# trunc, floor, ceil
+
+for T in (:Year, :Quarter, :Month, :Week, :Day, :Hour, :Minute, :Second, :Millisecond)
+  @eval begin
+    Base.trunc(nd::NanoDate, ::Type{$T}) = NanoDate(trunc(nd.datetime, $T))
+    Base.floor(nd::NanoDate, ::Type{$T}) = trunc(nd, $T)
+    Base.ceil(nd::NanoDate, ::Type{$T}) = NanoDate(ceil(nd.datetime), $T)
+end
+Base.ceil(nd::Nanodate, ::Type{Millisecond}) =
+    NanoDate(nd.datetime) + Millisecond(!iszero(nd.nanosecs))
+
+function Base.trunc(nd::NanoDate, ::Type{Microsecond})
+    cs, ns = fldmod(value(nd.nanosecs), NanosecondsPerMicrosecond)
+    NanoDate(nd.datetime, nanosecs(Microsecond(cs)))
+end
+Base.floor(nd::NanoDate, ::Type{Microsecond}) = trunc(nd, Microsecond)
+function Base.ceil(nd::NanoDate, ::Type{Microsecond})
+    cs, ns = fldmod(value(nd.nanosecs), NanosecondsPerMicrosecond)
+    NanoDate(nd.datetime, nanosec(Microsecond(cs))) + Microsecond(!iszero(ns))
+end
+
+Base.trunc(nd::NanoDate, ::Type{Nanosecond}) = nd
+Base.floor(nd::NanoDate, ::Type{Nanosecond}) = nd
+Base.ceil(nd::NanoDate, ::Type{Nanosecond}) = nd
+
+# rounding
+
+for T in (:Year, :Quarter, :Month, :Week, :Day, :Hour, :Minute, :Second,
+            :Millisecond, :Microsecond)
+  @eval begin
+    Base.round(nd::NanoDate, ::Type{$T}, ::RoundingMode{:Down}) = floor(nd, $T)
+    Base.round(nd::NanoDate, :Type{$T}, ::RoundingMode{:Up}) ) = ceil(nd, $T)
+    function Base.round(nd::NanoDate, :Type{$T}, ::RoundingMode{NearestTiesUp}))
+        up = round(nd, $T, RoundUp)
+        dn = round(nd, $T, RoundDown)
+        (up - nd) <= (nd - down) ? up : down
+    end
+    Base.round(nd::NanoDate, ::Type{$T}) = round(nd, $T, RoundNearestTiesUp)    
+  end
+end
+
+Base.round(nd::NanoDate, ::Type{Nanosecond}, ::RoundingMode{:Down}) = nd
+Base.round(nd::NanoDate, ::Type{Nanosecond}, ::RoundingMode{:Up}) = nd
+Base.round(nd::NanoDate, ::Type{Nanosecond}, ::RoundingMode{:RoundNearestTiesUp}) = nd
+
+
+
+
     

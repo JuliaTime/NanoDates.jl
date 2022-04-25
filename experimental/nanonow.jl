4b85c7f58ca13a4ac0f069ce276d4316ad247e3e
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-25 11:07:54 -0400

    offset

---
+++ experimental/nanonow.jl
@@ -0,0 +1,25 @@
+const u3 = UInt32(1_000)
+const u6 = UInt32(1_000_000)
+const u9 = UInt32(1_000_000_000)
+
+@inline millisecond_ns() = div(rem(time_ns(), u9), u6) 
+
+@inline millisecond_offset() = mod(value(now())%UInt64, u3) - millisecond_ns()
+
+@inline millisecond_ns(tm) = div(rem(tm, u9), u6) 
+
+@inline millisecond_offset(tm, dtm) = mod(value(dtm)%UInt64, u3) - millisecond_ns(tm)
+
+@inline function more_precise()
+    tm, dtm = time_ns(), Dates.value(now())%UInt64
+    tm_csns = mod(tm, u6)
+    # milli_offset = millisecond_offset(tm, dtm)
+    NanoDate(DateTime(Dates.UTM(dtm)), Nanosecond(tm_csns))
+end
+
+value(now()) * 1_000 - rem(time_ns(), u6)
+
+@inline dynmicoffset() = mod( time_ns(), 60_000_000_000), mod(value(now()), 60_000) * 1_000_000
+
+@inline offset(ns,dtm) = 
+    mod(ns, 60_000_000_000), mod(dtm, 60_000) * 1_000_000

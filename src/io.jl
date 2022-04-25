560b7e93195740453ae98d42ad382e88d3b78536
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-25 07:39:36 -0400

    io

--- src/io.jl
+++ src/io.jl
@@ -1,9 +1,9 @@
-Base.show(io::IO, nd::NanoDate; sep::CharString="")
+Base.show(io::IO, nd::NanoDate; sep="")
     str = string(nd; sep)
     print(io, str)
 end
 
-Base.show(io::IO, nd::NanoDate, df::DateFormat; sep::CharString="")
+Base.show(io::IO, nd::NanoDate, df::DateFormat; sep="")
     str = Dates.format(nd, df; sep)
     print(io, str)
 end

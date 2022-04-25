8caa864b527a97fca802e4ee5220866084483cde
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-25 03:31:49 -0400

    test

--- test/arith.jl
+++ test/arith.jl
@@ -1,6 +1,6 @@
 @testset "add periods" begin
-    @test ananodate + Year(1) == NanoDate(yr+1,mn,dy,hr+1,mi,sc,ms,cs,ns)
-    @test ananodate + Month(1) == NanoDate(yr,mn+1,dy,hr+1,mi,sc,ms,cs,ns)
+    @test ananodate + Year(1) == NanoDate(yr+1,mn,dy,hr,mi,sc,ms,cs,ns)
+    @test ananodate + Month(1) == NanoDate(yr,mn+1,dy,hr,mi,sc,ms,cs,ns)
     @test ananodate + Day(1) == NanoDate(yr,mn,dy+1,hr,mi,sc,ms,cs,ns)
     @test ananodate + Hour(1) == NanoDate(yr,mn,dy,hr+1,mi,sc,ms,cs,ns)
     @test ananodate + Minute(1) == NanoDate(yr,mn,dy,hr,mi+1,sc,ms,cs,ns)
@@ -11,8 +11,8 @@
 end
 
 @testset "subtract periods" begin
-    @test ananodate - Year(1) == NanoDate(yr-1,mn,dy,hr+1,mi,sc,ms,cs,ns)
-    @test ananodate - Month(1) == NanoDate(yr,mn-1,dy,hr+1,mi,sc,ms,cs,ns)
+    @test ananodate - Year(1) == NanoDate(yr-1,mn,dy,hr,mi,sc,ms,cs,ns)
+    @test ananodate - Month(1) == NanoDate(yr,mn-1,dy,hr,mi,sc,ms,cs,ns)
     @test ananodate - Day(1) == NanoDate(yr,mn,dy-1,hr,mi,sc,ms,cs,ns)
     @test ananodate - Hour(1) == NanoDate(yr,mn,dy,hr-1,mi,sc,ms,cs,ns)
     @test ananodate - Minute(1) == NanoDate(yr,mn,dy,hr,mi-1,sc,ms,cs,ns)

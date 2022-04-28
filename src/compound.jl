b644774dd4c5d7df540ce127993f5a994dc2d6ba
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-28 18:04:03 -0400

    CompoundPeriods

--- src/compound.jl
+++ src/compound.jl
@@ -1,6 +1,8 @@
-Base.iterate(cperiod::CompoundPeriod) = iterate(cperiod.periods)
+# reverse to process smaller period types before larger period types
+
+Base.iterate(cperiod::CompoundPeriod) = iterate(reverse(cperiod.periods))
  
-Base.iterate(cperiod::CompoundPeriod, state) = iterate(cperiod.periods, state)
+Base.iterate(cperiod::CompoundPeriod, state) = iterate(reverse(cperiod.periods), state)
 
 function Base.:(+)(nd::NanoDate, cperiod::CompoundPeriod)
     for p in cperiod
@@ -27,9 +29,65 @@ function Base.convert(::Type{CompoundPeriod}, tm::Time)
     Millisecond(millis) + Microsecond(micros) + Nanosecond(nanos)
 end
 
-Base.(+)(nd::NanoDate, tm::Time) =
+Base.convert(::Type{CompoundPeriod}, dt::Date)
+    yr,mn,dy = yearmonthday(dt)
+    Year(yr) + Month(mn) + Day(dy)
+end
+
+Base.convert(::Type{CompoundPeriod}, dtm::DateTime) =
+    convert(CompoundPeriod, Date(dtm)) +
+    convert(CompoundPeriod, Time(dtm))
+end
+
+Base.convert(::Type{CompoundPeriod}, nd::NanoDate) =
+    convert(CompoundPeriod, Date(nd)) +
+    convert(CompoundPeriod, Time(nd))
+end
+
+Base.convert(::Type{Time}, cperiod::CompoundPeriod)
+    cp = canonicalize(cperiod)
+    tm = Time0
+    for p in cp.periods
+        typeof(p) in (Year, Quarter, Month, Week, Day) && continue
+        tm += p
+    end
+    tm
+end
+
+Base.convert(::Type{Date}, cperiod::CompoundPeriod)
+    cp = canonicalize(cperiod)
+    dt = Date0
+    for p in cp.periods
+        typeof(p) in (Hour, Minute, Second, Millisecond, Microsecond, Nanosecond) && continue
+        dt += p
+    end
+    dt
+end
+
+Base.convert(::Type{DateTime}, cperiod::CompoundPeriod)
+    cp = canonicalize(cperiod)
+    dtm = DateTime0
+    for p in cp.periods
+        typeof(p) in (Microsecond, Nanosecond) && continue
+        dtm += p
+    end
+    dtm
+end
+
+Base.convert(::Type{NanoDate}, cperiod::CompoundPeriod)
+    cp = canonicalize(cperiod)
+    nd = NanoDate0
+    for p in cp.periods
+        nd += p
+    end
+    dtm
+end
+
+
+Base.:(+)(nd::NanoDate, tm::Time) =
     nd + convert(CompoundPeriod, tm)
 
-Base.(-)(nd::NanoDate, tm::Time) =
-    nd - convert(CompoundPeriod, tm)
+Base.:(-)(nd::NanoDate, tm::Time) =
+    nd + (-convert(CompoundPeriod, tm))
+
 

8447794a5185de0772aebe6399bbfee92b4c63e1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-04-29 01:19:01 -0400

    Delete src directory

--- src/io.jl
+++
@@ -1,9 +0,0 @@
-function Base.show(io::IO, nd::NanoDate; sep::CharString="")
-    str = string(nd; sep)
-    print(io, str)
-end
-
-function Base.show(io::IO, nd::NanoDate, df::DateFormat; sep::CharString="")
-    str = Dates.format(nd, df; sep)
-    print(io, str)
-end

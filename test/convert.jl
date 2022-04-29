518bd85f7909a56bbfd83fdb8998820a31040b16
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-29 00:29:56 -0400

    dev

--- test/convert.jl
+++ test/convert.jl
@@ -18,3 +18,20 @@ end
     @test NanoDate(ananodate, Microsecond(888), Nanosecond(444)) ==
         NanoDate(ananodate.datetime, nanosecs(Microsecond(888),Nanosecond(444)))
 end
+
+@testset "substitute" begin
+    @test ananodate - NanoDate(ananodate, Yr-Year(2)) == Year(2)
+    @test ananodate - NanoDate(ananodate, Quarter(2)) == Quarter(ananodate) - Quarter(2)
+    @test ananodate - NanoDate(ananodate, Mn-Month(-2)) == Month(-2)
+    @test ananodate - NanoDate(ananodate, Week(2)) == Week(ananodate) - Week(2)
+    @test ananodate - NanoDate(ananodate, Dy+Day(2)) == -Day(2)
+ 
+    @test ananodate - NanoDate(ananodate, Hr-Hour(2)) == Hour(2)
+    @test ananodate - NanoDate(ananodate, Mi+Minute(122)) == -Minute(122)
+    @test ananodate - NanoDate(ananodate, Sc-Second(64)) == Second(64)
+    @test ananodate - NanoDate(ananodate, Ms-Millisecond(324)) == Millisecond(324)
+
+    @test ananodate - NanoDate(ananodate, Cs-Microsecond(800)) == Microsecond(800)
+    @test ananodate - NanoDate(ananodate, Ns-Nanosecond(999)) == Nanosecond(999)
+end
+

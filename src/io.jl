1f6755c1afc9c4d87d4a351d13ea3a43f06b147c
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-04-29 23:36:51 -0400

    str

--- src/io.jl
+++ src/io.jl
@@ -3,14 +3,24 @@ function Base.show(io::IO, nd::NanoDate; sep::CharString="")
     print(io, str)
 end
 
-function Base.show(io::IO, nd::NanoDate, df::DateFormat; sep::CharString="")
+function Base.show(io::IO, nd::NanoDate,
+                    df::DateFormat; sep::CharString="")
     str = Dates.format(nd, df; sep)
     print(io, str)
 end
 
 
-# this is used to handle a call to `print`
-Base.show(io::IO, nd::NanoDate) = print(io, string(nd))
+Base.show(io::IO, nd::NanoDate) = 
+    Base.show(io::IO, m::MIME"text/plain", nd::NanoDate)
 
-# this is used to show values in the REPL and when using IJulia
-Base.show(io::IO, m::MIME"text/plain", nd::NanoDate) = print(io, string(nd))
+Base.show(io::IO, m::MIME"text/plain", nd::NanoDate) =
+    print(io, string(nd))
+
+Base.show(io::IO, nd::NanoDate, sep::Tuple{Vararg{Char}}) = 
+    Base.show(io::IO, m::MIME"text/plain", nd::NanoDate, sep)
+
+# to show values in the REPL and when using IJulia
+Base.show(io::IO, m::MIME"text/plain", 
+          nd::NanoDate, sep::Tuple{Vararg{Char}}) =
+    print(io, string(nd, sep))
+

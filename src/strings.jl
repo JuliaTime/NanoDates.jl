1013560f0691271d0aa5ebbb984c4524e7e5b4e3
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-05-02 21:27:54 -0400

    string

--- src/strings.jl
+++ src/strings.jl
@@ -80,6 +80,11 @@ function nanodate_format(nd, df, sep)
     iszero(nanos) && return str
     cs, ns = divrem(nanos, 1_000)
     millis = millisecond(nd.datetime)
+    if millis < 10
+       str = str * "00"
+    elseif millis < 100
+       str = str * "0"
+    end
     if nsubsecfields > 1
         str = str * sep * lpad(cs, 3, '0')
         iszero(ns) && return str

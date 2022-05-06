1b7900eb50cb7b87c50fb9b6fc6e7420f99c0e47
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-05-06 04:21:58 -0400

    Create intmath.jl

---
+++ src/intmath.jl
@@ -0,0 +1,42 @@
+
+
+
+
+
+#=
+
+references
+
+Euclidean Affine Functions and Applications to Calendar Algorithms
+Cassio Neri, Lorenz Schneider
+arXiv:2102.06959v1 [cs.DS] 13 Feb 2021
+
+=#
+
+"""
+    divmod10(x)
+
+div(x,10) x  ÷  10
+rem(x,10) 
+mod(x,10)
+"""
+#=
+    div10(n) = 
+    div10(n) = 
+=#
+function divmod10(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

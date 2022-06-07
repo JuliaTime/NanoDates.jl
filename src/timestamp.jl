f49644233e79589031a027d8c8a7e0710d544c5f
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-07 13:02:24 -0400

    improve utc_delta

--- src/timestamp.jl
+++ src/timestamp.jl
@@ -5,20 +5,23 @@ val(x) = isempty(x) ? 0 : value(x)
 
 function utc_delta()
     utctime, localtime = now(UTC), now()
-    delta_minutes = floor(localtime - utctime, Minute)
-    delta_hour = floor(delta_minutes, Hour)
-    delta_hour_value = value(delta_hour)
-    delta_minute = canonicalize(delta_minutes - delta_hour)
-    delta_minute_value = val(delta_minute)
+    i = 6
+    while (minute(utctime)  != minute(localtime)) && i > 0
+        utctime, localtime = now(UTC), now()
+        i -= 1
+    end
+    delta_time = canonicalize((Hour(localtime) + Minute(localtime)) - (Hour(utctime) + Minute(utctime)))
+    delta_hours = Hour(delta_time)
+    delta_minutes = Minute(delta_time)
     delta_sign = '+'
-    if delta_hour_value < 0
+    if value(delta_hours) < 0
         delta_sign = '-'
     end
-    delta_hour_str = string(abs(delta_hour_value))
+    delta_hour_str = string(abs(value(delta_hours)))
     if abs(delta_hour_value) < 10
        delta_hour_str = "0" * delta_hour_str
     end
-    delta_minute_str = string(delta_minute_value)
+    delta_minute_str = string(abs(value(delta_minutes)))
     if length(delta_minute_str) < 2
         delta_minute_str = "0" * delta_minute_str
     end

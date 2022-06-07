3318d93adb64bf2117ce8d033e66cbbbab659f2f
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-07 01:45:18 -0400

    whitespace

--- test/compound.jl
+++ test/compound.jl
@@ -1,12 +1,9 @@
 @testset "CompoundPeriod" begin
-
   @test dnd == Day(91) + Nanosecond(28656000000000)
-  @test cdnd == Day(91) + Hour(7) + Minute(57) + Second(36) 
-
+  @test cdnd == Day(91) + Hour(7) + Minute(57) + Second(36)
 end
 
 @testset "within CompoundPeriods"
-
   @test hour(cdnd) == 7
   @test Day(dnd) == Day(19)
 
@@ -15,5 +12,4 @@ end
 
   @test week(dnd) == Week(0)
   @test week(canonicalize(dnd)) == 13
-
 end

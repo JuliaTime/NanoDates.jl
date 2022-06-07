4056a92f53ead17284b022f55f3cd8e3174fb8c8
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-07 01:47:07 -0400

    insert missing begin

--- test/compound.jl
+++ test/compound.jl
@@ -1,9 +1,12 @@
 @testset "CompoundPeriod" begin
+
   @test dnd == Day(91) + Nanosecond(28656000000000)
   @test cdnd == Day(91) + Hour(7) + Minute(57) + Second(36)
+
 end
 
-@testset "within CompoundPeriods"
+@testset "within CompoundPeriods" begin
+
   @test hour(cdnd) == 7
   @test Day(dnd) == Day(19)
 
@@ -12,4 +15,5 @@ end
 
   @test week(dnd) == Week(0)
   @test week(canonicalize(dnd)) == 13
+
 end

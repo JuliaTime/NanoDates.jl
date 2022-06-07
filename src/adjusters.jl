0a6f3bdc6efd3d4d2cdec7056768c35a50a5d97e
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-07 02:42:42 -0400

    firstof

--- src/adjusters.jl
+++ src/adjusters.jl
@@ -1,13 +1,26 @@
 ### trunction
 
-Base.trunc(dt::NanoDate, p::Type{Year}) = NanoDate(trunc(Date(dt), Year))
-Base.trunc(dt::NanoDate, p::Type{Quarter}) = NanoDate(trunc(Date(dt), Quarter))
-Base.trunc(dt::NanoDate, p::Type{Month}) = NanoDate(trunc(Date(dt), Month))
-Base.trunc(dt::NanoDate, p::Type{Day}) = NanoDate(Date(dt))
-Base.trunc(dt::NanoDate, p::Type{Hour}) = dt - Minute(dt) - Second(dt) - Millisecond(dt)
-Base.trunc(dt::NanoDate, p::Type{Minute}) = dt - Second(dt) - Millisecond(dt)
-Base.trunc(dt::NanoDate, p::Type{Second}) = dt - Millisecond(dt)
-Base.trunc(dt::NanoDate, p::Type{Millisecond}) = dt - div(dt.nanosecs, NanosecondsPerMillisecond)
-Base.trunc(dt::NanoDate, p::Type{Microsecond}) = dt - rem(dt.nanosecs, NanosecondsPerMicrosecond)
-Base.trunc(dt::NanoDate, p::Type{Nanosecond}) = dt
+Base.trunc(nd::NanoDate, p::Type{Year}) = NanoDate(trunc(Date(nd), Year))
+Base.trunc(nd::NanoDate, p::Type{Quarter}) = NanoDate(trunc(Date(nd), Quarter))
+Base.trunc(nd::NanoDate, p::Type{Month}) = NanoDate(trunc(Date(nd), Month))
+Base.trunc(nd::NanoDate, p::Type{Day}) = NanoDate(Date(nd))
+Base.trunc(nd::NanoDate, p::Type{Hour}) = nd - (Minute(nd) + Second(nd) + Millisecond(nd))
+Base.trunc(nd::NanoDate, p::Type{Minute}) = nd - (Second(nd) + Millisecond(nd))
+Base.trunc(nd::NanoDate, p::Type{Second}) = nd - Millisecond(nd)
+Base.trunc(nd::NanoDate, p::Type{Millisecond}) = nd - div(nd.nanosecs, NanosecondsPerMillisecond)
+Base.trunc(nd::NanoDate, p::Type{Microsecond}) = nd - rem(nd.nanosecs, NanosecondsPerMicrosecond)
+Base.trunc(nd::NanoDate, p::Type{Nanosecond}) = nd
 
+### firstof
+
+firstdayofyear(nd::NanoDate) = NanoDate(firstdayofyear(nd.datetime))
+firstdayofquarter(nd::NanoDate) = NanoDate(firstdayofquarter(nd.datetime))
+firstdayofmonth(nd::NanoDate) = NanoDate(firstdayofmonth(nd.datetime))
+firstdayofweek(nd::NanoDate) = NanoDate(firstdayofweek(nd.datetime))
+
+firsthourofday(nd::NanoDate) = trunc(nd, Day)
+firstminuteofhour(nd::NanoDate) = trunc(nd, Hour)
+firstsecondofminute(nd::NanoDate) = trunc(nd, Minute)
+firstmillisecondofsecond(nd::NanoDate) = trunc(nd, Second)
+firstmicrosecondofmillisecond(nd::NanoDate) = trunc(nd, Millisecond)
+firstnanosecondofmicrosecond(nd::NanoDate) = trunc(nd, Microsecond)
---
+++ test/adjusters.jl
@@ -0,0 +1,15 @@
+@testset "adjusters .. truncation" begin
+
+   trunc(nd, Year) == firstdayofyear(nd)
+   trunc(nd, Quarter) == firstdayofquarter(nd)
+   trunc(nd, Month) == firstdayofmonth(nd))
+   trunc(nd, Week) == firstdayofweek(nd)
+   trunc(nd, Day) == firsthourofday(nd)
+   trunc(nd, Hour) == firstminuteofhour(nd)
+   trunc(nd, Minute) == firstsecondofminute(nd)
+   trunc(nd, Second) = firstmillisecondofsecond(nd)
+   trunc(nd, Millisecond) = firstmillisecondofsecond(nd)
+   trunc(nd, Microsecond) = firstmicrosecondofmillisecond(nd)
+   trunc(nd, Nanosecond) = firstnanosecondofmicrosecond(nd)
+   
+end

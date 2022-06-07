0bad7b00e42dec64cc8803725aeb32486c1866f4
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-07 04:57:14 -0400

    fix test

--- test/strings.jl
+++ test/strings.jl
@@ -8,7 +8,7 @@ end
 @testset "parse" begin
     
     @test parse(NanoDate, "1999-07-12 08:15:30", dateformat"yyyy-mm-dd HH:MM:SS") == NanoDate(1999,07,12,08,15,30)
-    @test parse(NanoDate, "2022-7-8 4:5:6.1", dateformat"yyyy-m-d H:M:S.s") == NanoDate(2022,7,8,4,5,6,1)
+    @test parse(NanoDate, "2022-7-8 4:5:6.1", dateformat"yyyy-m-d H:M:S.s") == NanoDate(2022,7,8,4,5,6,100)
     @test parse(NanoDate, "2022/07/18 04:15:16.124", dateformat"yyyy/mm/dd HH:MM:SS.s") == NanoDate(2022,7,18,4,15,16,124)
     @test parse(NanoDate, "2022-07-18T04:15:16.1245", dateformat"yyyy-mm-ddTHH:MM:SS.ss") == NanoDate(2022,7,18,4,15,16,124,500)
     @test parse(NanoDate, "2022-07-18T04:15:16.12456789", dateformat"yyyy-mm-ddTHH:MM:SS.sss") == NanoDate(2022,7,18,4,15,16,124,567,890)

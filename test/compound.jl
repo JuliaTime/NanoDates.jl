cdde44278ef6ff3985670697486580dc532cbc3d
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-07 02:54:59 -0400

    coverage

--- test/compound.jl
+++ test/compound.jl
@@ -1,8 +1,15 @@
 @testset "CompoundPeriod" begin
 
-  @test Day(Day(5)) == Day(5)
-  @test hour(Hour(5)) == 5
+  @test Month(Month(5)) == Month(5)
+  @test Day(Day(15)) == Day(15)
+  @test Hour(Hour(1)) == Hour(1)
+  @test Nanosecond(Nanosecond(123)) == Nanosecond(123)
 
+  @test month(Month(5)) == 5
+  @test day(Day(15)) == 15
+  @test hour(Hour(1)) == 1
+  @test nanosecond(Nanosecond(123)) == 123
+  
   @test dnd == Day(91) + Nanosecond(28656000000000)
   @test cdnd == Day(91) + Hour(7) + Minute(57) + Second(36)
 

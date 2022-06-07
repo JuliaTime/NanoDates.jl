00030907cff6d863aedf05da13110b744d55eb05
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-07 02:47:25 -0400

    firstof

--- src/adjusters.jl
+++ src/adjusters.jl
@@ -1,16 +1,3 @@
-### trunction
-
-Base.trunc(nd::NanoDate, p::Type{Year}) = NanoDate(trunc(Date(nd), Year))
-Base.trunc(nd::NanoDate, p::Type{Quarter}) = NanoDate(trunc(Date(nd), Quarter))
-Base.trunc(nd::NanoDate, p::Type{Month}) = NanoDate(trunc(Date(nd), Month))
-Base.trunc(nd::NanoDate, p::Type{Day}) = NanoDate(Date(nd))
-Base.trunc(nd::NanoDate, p::Type{Hour}) = nd - (Minute(nd) + Second(nd) + Millisecond(nd))
-Base.trunc(nd::NanoDate, p::Type{Minute}) = nd - (Second(nd) + Millisecond(nd))
-Base.trunc(nd::NanoDate, p::Type{Second}) = nd - Millisecond(nd)
-Base.trunc(nd::NanoDate, p::Type{Millisecond}) = nd - div(nd.nanosecs, NanosecondsPerMillisecond)
-Base.trunc(nd::NanoDate, p::Type{Microsecond}) = nd - rem(nd.nanosecs, NanosecondsPerMicrosecond)
-Base.trunc(nd::NanoDate, p::Type{Nanosecond}) = nd
-
 ### firstof
 
 firstdayofyear(nd::NanoDate) = NanoDate(firstdayofyear(nd.datetime))

e8d4b3335718e2251c68f991a216a8debaea9641
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-18 11:29:56 -0400

    resolve firstof_/lastof_

--- src/adjusters.jl
+++ src/adjusters.jl
@@ -1,9 +1,9 @@
 ### firstof
 
-firstdayofyear(nd::NanoDate) = NanoDate(firstdayofyear(nd.datetime))
-firstdayofquarter(nd::NanoDate) = NanoDate(firstdayofquarter(nd.datetime))
-firstdayofmonth(nd::NanoDate) = NanoDate(firstdayofmonth(nd.datetime))
-firstdayofweek(nd::NanoDate) = NanoDate(firstdayofweek(nd.datetime))
+Dates.firstdayofyear(nd::NanoDate) = NanoDate(firstdayofyear(nd.datetime))
+Dates.firstdayofquarter(nd::NanoDate) = NanoDate(firstdayofquarter(nd.datetime))
+Dates.firstdayofmonth(nd::NanoDate) = NanoDate(firstdayofmonth(nd.datetime))
+Dates.firstdayofweek(nd::NanoDate) = NanoDate(firstdayofweek(nd.datetime))
 
 firsthourofday(nd::NanoDate) = trunc(nd, Day)
 firstminuteofhour(nd::NanoDate) = trunc(nd, Hour)
@@ -14,10 +14,10 @@ firstnanosecondofmicrosecond(nd::NanoDate) = trunc(nd, Microsecond)
 
 ### last of
 
-lastdayofyear(nd::NanoDate) = NanoDate(lastdayofyear(nd.datetime))
-lastdayofquarter(nd::NanoDate) = NanoDate(lastdayofquarter(nd.datetime))
-lastdayofmonth(nd::NanoDate) = NanoDate(lastdayofmonth(nd.datetime))
-lastdayofweek(nd::NanoDate) = NanoDate(lastdayofweek(nd.datetime))
+Dates.lastdayofyear(nd::NanoDate) = NanoDate(lastdayofyear(nd.datetime))
+Dates.lastdayofquarter(nd::NanoDate) = NanoDate(lastdayofquarter(nd.datetime))
+Dates.lastdayofmonth(nd::NanoDate) = NanoDate(lastdayofmonth(nd.datetime))
+Dates.lastdayofweek(nd::NanoDate) = NanoDate(lastdayofweek(nd.datetime))
 
 lasthourofday(nd::NanoDate) = trunc(nd, Day) + Hour(23)
 lastminuteofhour(nd::NanoDate) = trunc(nd, Hour) + Minute(59)

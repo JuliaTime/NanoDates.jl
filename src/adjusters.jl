478394fd397acb9975205d18df6362787bdf30f0
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-18 10:53:21 -0400

    add lastofs

--- src/adjusters.jl
+++ src/adjusters.jl
@@ -11,3 +11,17 @@ firstsecondofminute(nd::NanoDate) = trunc(nd, Minute)
 firstmillisecondofsecond(nd::NanoDate) = trunc(nd, Second)
 firstmicrosecondofmillisecond(nd::NanoDate) = trunc(nd, Millisecond)
 firstnanosecondofmicrosecond(nd::NanoDate) = trunc(nd, Microsecond)
+
+### last of
+
+lastdayofyear(nd::NanoDate) = NanoDate(lastdayofyear(nd.datetime))
+lastdayofquarter(nd::NanoDate) = NanoDate(lastdayofquarter(nd.datetime))
+lastdayofmonth(nd::NanoDate) = NanoDate(lastdayofmonth(nd.datetime))
+lastdayofweek(nd::NanoDate) = NanoDate(lastdayofweek(nd.datetime))
+
+lasthourofday(nd::NanoDate) = trunc(nd, Day) + Hour(23)
+lastminuteofhour(nd::NanoDate) = trunc(nd, Hour) + Minute(59)
+lastsecondofminute(nd::NanoDate) = trunc(nd, Minute) + Second(59)
+lastmillisecondofsecond(nd::NanoDate) = trunc(nd, Second) + Millisecond(999)
+lastmicrosecondofmillisecond(nd::NanoDate) = trunc(nd, Millisecond) + Microsecond(999)
+lastnanosecondofmicrosecond(nd::NanoDate) = trunc(nd, Microsecond) + Nanosecond(999)

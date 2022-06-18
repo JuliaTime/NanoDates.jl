48ac28edb57ff75a727721bdaa97567d7d55fdd9
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-18 11:22:53 -0400

    reorg tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,37 +13,36 @@ Aqua.test_deps_compat(NanoDates)
 
 include("constants.jl")
 
-@testset "nanodate" begin
-    include("nanodate.jl")
-end
-
-    @test days(nd) == days(nd.datetime)
-    @test yearmonthday(nd) == (year(nd), month(nd), day(nd)) 
-    @test yearmonth(nd) == (year(nd), month(nd))
-    @test monthday(nd) == (month(nd), day(nd))
-    
 @testset "accessors" begin
     include("accessors.jl")
 end
 
-@testset "convert" begin
-    include("convert.jl")
-end
-
-@testset "compare" begin
-    include("compare.jl")
-end
-
-@testset "interop" begin
-    include("interop.jl")
+@testset "adjusters" begin
+    include("adjusters.jl")
 end
 
 @testset "arith" begin
     include("arith.jl")
 end
 
+@testset "compare" begin
+    include("compare.jl")
+end
+
 @testset "compound" begin
     include("compound.jl")
+end
+
+@testset "convert" begin
+    include("convert.jl")
+end
+
+@testset "interop" begin
+    include("interop.jl")
+end
+
+@testset "nanodate" begin
+    include("nanodate.jl")
 end
 
 @testset "strings" begin

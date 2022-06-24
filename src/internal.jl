06ee3232127881640b58f451ba04171bb3a41844
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-24 16:13:23 -0400

    mre

--- src/internal.jl
+++ src/internal.jl
@@ -1,6 +1,7 @@
 
 using Dates, NanoDates, Parsers;
 
+
 translation_keys = collect(keys(Dates.CONVERSION_TRANSLATIONS));
 push!(translation_keys, NanoDates.NanoDate);
 
@@ -21,3 +22,182 @@ function Dates.validargs(::Type{NanoDate}, year::Int64=year(today()),
                                 millisecond, microsecond, nanosecond)
 end
 
+
+# fallback that would call custom DatePart overloads that are expecting a string
+function tryparsenext(tok, source, pos, len, b, code)::Tuple{Any, Int, UInt8, ReturnCode}
+    strlen = min(len - pos + 1, 64)
+    str = getstring(source, PosLen(pos, strlen), 0x00)
+    res = Dates.tryparsenext(tok, str, 1, strlen)
+    if res === nothing
+        val = nothing
+        code |= INVALID_TOKEN
+    else
+        val, i = res
+        pos += i - 1
+        if eof(source, pos, len)
+            code |= EOF
+        else
+            b = peekbyte(source, pos)
+        end
+    end
+    return val, pos, b, code
+end
+
+@inline function typeparser(::Type{T}, source, pos, len, b, code, options) where {T<:NanoDate}
+    fmt = options.dateformat
+    df = fmt === nothing ? default_format(T) : fmt
+    tokens = df.tokens
+    locale::Dates.DateLocale = df.locale
+    year = month = day = Int64(1)
+    hour = minute = second = millisecond = Int64(0)
+    tz = ""
+    ampm = Dates.TWENTYFOURHOUR
+    
+    extras = nothing
+    for tok in tokens
+        # @show pos, Char(b), code, typeof(tok)
+        eof(code) && break
+        if tok isa Delim{Char}
+            pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Delim{String}
+            pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'y'}
+            year, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'Y'}
+            year, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'m'}
+            month, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'u'}
+            month, pos, b, code = tryparsenext(tok, source, pos, len, b, code, locale)
+        elseif tok isa Dates.DatePart{'U'}
+            month, pos, b, code = tryparsenext(tok, source, pos, len, b, code, locale)
+        elseif tok isa Dates.DatePart{'d'}
+            day, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'e'}
+            _, pos, b, code = tryparsenext(tok, source, pos, len, b, code, locale)
+        elseif tok isa Dates.DatePart{'E'}
+            _, pos, b, code = tryparsenext(tok, source, pos, len, b, code, locale)
+        elseif tok isa Dates.DatePart{'H'}
+            hour, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'I'}
+            hour, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'M'}
+            minute, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'S'}
+            second, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'p'}
+            ampm, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'s'}
+            millisecond, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'z'}
+            tz, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'Z'}
+            tz, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        else
+            # non-Dates defined character code
+            # allocate extras if not already and parse
+            if extras === nothing
+                extras = Dict{Type, Any}()
+            end
+            extraval, pos, b, code = tryparsenext(tok, source, pos, len, b, code)::Tuple{Any, Int, UInt8, ReturnCode}
+            extras[Dates.CONVERSION_SPECIFIERS[charactercode(tok)]] = extraval
+        end
+        if invalid(code)
+            if invalidtoken(code)
+                code &= ~INVALID_TOKEN
+            end
+            break
+        end
+        # @show pos, Char(b), code
+    end
+
+    valid = Dates.validargs(T, year, month, day, hour, minute, second, millisecond, microsecond, nanosecond)
+    if eof(source, pos, len)
+        code |= EOF
+    end
+    return x, code, pos
+end
+
+
+nd = NanoDate(2022, 4, 27,  12, 21, 0,  123, 456, 789); ndstr=string(nd);
+
+
+
+@inline function typeparser(::Type{NanoDate}, source, pos, len, b, code, options)
+    T = NanoDate
+    fmt = options.dateformat
+    df = fmt === nothing ? default_format(T) : fmt
+    tokens = df.tokens
+    locale::Dates.DateLocale = df.locale
+    year = month = day = Int64(1)
+    hour = minute = second = millisecond = Int64(0)
+    tz = ""
+    ampm = Dates.TWENTYFOURHOUR
+    
+    extras = nothing
+    for tok in tokens
+        # @show pos, Char(b), code, typeof(tok)
+        eof(code) && break
+        if tok isa Delim{Char}
+            pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Delim{String}
+            pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'y'}
+            year, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'Y'}
+            year, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'m'}
+            month, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'u'}
+            month, pos, b, code = tryparsenext(tok, source, pos, len, b, code, locale)
+        elseif tok isa Dates.DatePart{'U'}
+            month, pos, b, code = tryparsenext(tok, source, pos, len, b, code, locale)
+        elseif tok isa Dates.DatePart{'d'}
+            day, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'e'}
+            _, pos, b, code = tryparsenext(tok, source, pos, len, b, code, locale)
+        elseif tok isa Dates.DatePart{'E'}
+            _, pos, b, code = tryparsenext(tok, source, pos, len, b, code, locale)
+        elseif tok isa Dates.DatePart{'H'}
+            hour, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'I'}
+            hour, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'M'}
+            minute, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'S'}
+            second, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'p'}
+            ampm, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'s'}
+            millisecond, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'z'}
+            tz, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'Z'}
+            tz, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        else
+            # non-Dates defined character code
+            # allocate extras if not already and parse
+            if extras === nothing
+                extras = Dict{Type, Any}()
+            end
+            extraval, pos, b, code = tryparsenext(tok, source, pos, len, b, code)::Tuple{Any, Int, UInt8, ReturnCode}
+            extras[Dates.CONVERSION_SPECIFIERS[charactercode(tok)]] = extraval
+        end
+        if invalid(code)
+            if invalidtoken(code)
+                code &= ~INVALID_TOKEN
+            end
+            break
+        end
+        # @show pos, Char(b), code
+    end
+
+    valid = Dates.validargs(T, year, month, day, hour, minute, second, millisecond, microsecond, nanosecond)
+    if eof(source, pos, len)
+        code |= EOF
+    end
+    return x, code, pos
+end
+
+
+nd = NanoDate(2022, 4, 27,  12, 21, 0,  123, 456, 789); ndstr=string(nd);

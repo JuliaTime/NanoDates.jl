f7a20a78d2ede68c844e96f9234193e7b6e7f336
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-24 15:16:48 -0400

    validargs

---
+++ src/internal.jl
@@ -0,0 +1,38 @@
+
+using Dates, NanoDates, Parsers;
+
+translation_keys = collect(keys(Dates.CONVERSION_TRANSLATIONS));
+push!(translation_keys, NanoDates.NanoDate);
+
+translation_values = collect(values(Dates.CONVERSION_TRANSLATIONS));
+push!(translation_values, (Year, Month, Day, Hour, Minute, Second, Millisecond, Microsecond, Nanosecond, Dates.AMPM));
+
+const CONVERSIONTRANSLATIONS = IdDict(zip(translation_keys, translation_values));
+
+
+function Dates.validargs(::Type{NanoDate}, year::Int64=year(today()), 
+                                           month::Int64=one(Int64), day::Int64=one(Int64),
+                                           hour::Int64=0, minute::Int64=0, second::Int64=0,
+                                           millisecond::Int64=0, microsecond::Int64=0,
+                                           nanosecond::Int64=0, ampm=nothing)
+
+    Dates.validargs(Dates.Date, year, month, day)
+
+    if !isnothing(ampm)
+        Dates.validargs(Dates.Date, year, month, day) 
+    else
+        Dates.validargs(Dates.Time, hour, minute, second, 
+                                    millisecond, microsecond, nanosecond)
+    end
+    
+    validargs 
+end
+
+function Dates.validargs(::Type{NanoDate}, year, month, day,
+                                           hour, minute, second,
+                                           millisecond, microsecond, nanosecond, ampm)
+
+    Dates.validargs(Dates.Date, year, month, day)
+    Dates.validargs(Dates.Time, hour, minute, second, 
+                                millisecond, microsecond, nanosecond, ampm) 
+end

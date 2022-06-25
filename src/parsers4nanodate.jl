71bd24179ef0cae2166d580d3c672c06f0e6b68a
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-25 07:27:29 -0400

    validargs

--- src/parsers4nanodate.jl
+++ src/parsers4nanodate.jl
@@ -7,7 +7,7 @@ export ISONanoDateFormat
 
 # importing what is not exported from Dates
 #   that is used in this source file
-import Dates: DatePart, Delim, validargs
+import Dates: DatePart, Delim, validargs, argerror
 
 import Parsers: tryparse, tryparsenext, tryparsenext_base10,
     Format, default_format, charactercode,
@@ -17,20 +17,20 @@ const ISONanoDateFormat = Format("yyyy-mm-dd\\THH:MM:SS.sss")
 Dates.default_format(::Type{NanoDate}) = ISONanoDateTimeFormat
 
 function Dates.validargs(::Type{NanoDate}, y::T, m::T, d::T, h::T, mi::T, s::T, ms::T, us::T, ns::T, ampm::Dates.AMPM=Dates.TWENTYFOURHOUR) where {T<:Union{Int64, Int128}}
-    0 < m < 13 || return argerror("Month: $m out of range (1:12)")
-    0 < d < daysinmonth(y, m) + 1 || return argerror("Day: $d out of range (1:$(daysinmonth(y, m)))")
+    0 < m < 13 || return Dates.argerror("Month: $m out of range (1:12)")
+    0 < d < daysinmonth(y, m) + 1 || return Dates.argerror("Day: $d out of range (1:$(daysinmonth(y, m)))")
 
     if ampm == Dates.TWENTYFOURHOUR # 24-hour clock
-        -1 < h < 24 || return argerror("Hour: $h out of range (0:23)")
+        -1 < h < 24 || return Dates.argerror("Hour: $h out of range (0:23)")
     else
-        0 < h < 13 || return argerror("Hour: $h out of range (1:12)")
+        0 < h < 13 || return Dates.argerror("Hour: $h out of range (1:12)")
     end
-    -1 < mi < 60 || return argerror("Minute: $mi out of range (0:59)")
-    -1 < s < 60 || return argerror("Second: $s out of range (0:59)")
-    -1 < ms < 1000 || return argerror("Millisecond: $ms out of range (0:999)")
-    -1 < us < 1000 || return argerror("Microsecond: $us out of range (0:999)")
-    -1 < ns < 1000 || return argerror("Nanosecond: $ns out of range (0:999)")
-    return argerror()
+    -1 < mi < 60 || return Dates.argerror("Minute: $mi out of range (0:59)")
+    -1 < s < 60 || return Dates.argerror("Second: $s out of range (0:59)")
+    -1 < ms < 1000 || return Dates.argerror("Millisecond: $ms out of range (0:999)")
+    -1 < us < 1000 || return Dates.argerror("Microsecond: $us out of range (0:999)")
+    -1 < ns < 1000 || return Dates.argerror("Nanosecond: $ns out of range (0:999)")
+    return Dates.argerror()
 end
 
 

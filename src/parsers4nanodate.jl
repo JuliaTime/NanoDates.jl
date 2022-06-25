ec8f37cb3de495350d5bfcad5b4b43330d70bff3
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-25 18:23:29 -0400

    typo

--- src/parsers4nanodate.jl
+++ src/parsers4nanodate.jl
@@ -14,7 +14,7 @@ import Parsers: tryparse, tryparsenext, tryparsenext_base10,
     Options, Result
 
 const ISONanoDateFormat = Format("yyyy-mm-dd\\THH:MM:SS.sss")
-Dates.default_format(::Type{NanoDate}) = ISONanoDateTimeFormat
+Dates.default_format(::Type{NanoDate}) = ISONanoDateFormat
 
 function Dates.validargs(::Type{NanoDate}, y::T, m::T, d::T, h::T, mi::T, s::T, ms::T, us::T, ns::T, ampm::Dates.AMPM=Dates.TWENTYFOURHOUR) where {T<:Union{Int64, Int128}}
     0 < m < 13 || return Dates.argerror("Month: $m out of range (1:12)")
@@ -36,6 +36,138 @@ end
 
 Base.getindex(collection::IdDict{Type,Any}, ::Type{NanoDate}) =
     (Year, Month, Day, Hour, Minute, Second, Millisecond, Microsecond, Nanosecond)
+
+    
+@inline function typeparser(::Type{NanoDate}, source, pos, len, b, code, options)
+    fmt = options.dateformat
+    df = fmt === nothing ? default_format(T) : fmt
+    tokens = df.tokens
+    locale::Dates.DateLocale = df.locale
+    year = month = day = Int64(1)
+    hour = minute = second = millisecond = microsecond = nanosecond = Int64(0)
+    tz = ""
+    ampm = Dates.TWENTYFOURHOUR
+    extras = nothing
+
+    pos, len, b, code = tryparsetokens(tokens, pos, len, b, code, locale, extras)
+
+function tryparsetokens(tokens, pos, len, b, code, locale)
+    for tok in tokens
+        # @show pos, Char(b), code, typeof(tok)
+        eof(code) && break
+        if tok isa Delim{Char}
+            pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Delim{String}
+            pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'y'}
+            year, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'Y'}
+            year, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'m'}
+            month, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'u'}
+            month, pos, b, code = tryparsenext(tok, source, pos, len, b, code, locale)
+        elseif tok isa Dates.DatePart{'U'}
+            month, pos, b, code = tryparsenext(tok, source, pos, len, b, code, locale)
+        elseif tok isa Dates.DatePart{'d'}
+            day, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'e'}
+            _, pos, b, code = tryparsenext(tok, source, pos, len, b, code, locale)
+        elseif tok isa Dates.DatePart{'E'}
+            _, pos, b, code = tryparsenext(tok, source, pos, len, b, code, locale)
+        elseif tok isa Dates.DatePart{'H'}
+            hour, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'I'}
+            hour, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'M'}
+            minute, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'S'}
+            second, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'p'}
+            ampm, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'s'}
+            if tok.width >= 1
+                millisecond, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+            end
+            if tok.width >= 2
+                microsecond, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+            end
+            if tok.width >= 3
+                nanosecond, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+            end
+        elseif tok isa Dates.DatePart{'z'}
+            tz, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        elseif tok isa Dates.DatePart{'Z'}
+            tz, pos, b, code = tryparsenext(tok, source, pos, len, b, code)
+        else
+            # non-Dates defined character code
+            # allocate extras if not already and parse
+            if extras === nothing
+                extras = Dict{Type,Any}()
+            end
+            extraval, pos, b, code = tryparsenext(tok, source, pos, len, b, code)::Tuple{Any,Int,UInt8,ReturnCode}
+            extras[Dates.CONVERSION_SPECIFIERS[charactercode(tok)]] = extraval
+        end
+    
+        if invalid(code)
+            if invalidtoken(code)
+                code &= ~INVALID_TOKEN
+            end
+            break
+        end
+        # @show pos, Char(b), code
+    end
+    pos, len, b, code
+end
+
+    valid = Dates.validargs(T, year, month, day, hour, minute, second, millisecond, microsecond, nanosecond, ampm)
+    elseif T.name.name === :ZonedDateTime
+        valid = Dates.validargs(T, year, month, day, hour, minute, second, millisecond, tz)
+    else
+        # custom TimeType
+        if extras === nothing
+            extras = Dict{Type,Any}()
+        end
+        extras[Year] = year
+        extras[Month] = month
+        extras[Day] = day
+        extras[Hour] = hour
+        extras[Minute] = minute
+        extras[Second] = second
+        extras[Millisecond] = millisecond
+        extras[Microsecond] = microsecond
+        extras[Nanosecond]  = nanosecond
+        types = Dates.CONVERSION_TRANSLATIONS[T]
+        vals = Vector{Any}(undef, length(types))
+        for (i, type) in enumerate(types)
+            vals[i] = get(extras, type) do
+                Dates.CONVERSION_DEFAULTS[type]
+            end
+        end
+        valid = Dates.validargs(T, vals...)
+    end
+    if invalid(code) || valid !== nothing
+        if T.name.name === :ZonedDateTime
+            x = T(0, TimeZone("UTC"))
+        else
+            x = T(0)
+        end
+        code |= INVALID
+    else
+        # xdate = Date(Dates.UTD(Dates.totaldays(year, month, day)))
+        # xtime = Time(Nanosecond(1000000 * millisecond + 1000000000 * second + 60000000000 * minute + 3600000000000 * (Dates.adjusthour(hour, ampm))))
+        # x = NanoDate(xdate, xtime)
+        xnanosecs = Nanosecond(microsecond * 1000 + nanosecond)
+        xdatetime = DateTime(Dates.UTM(millisecond + 1000 * (second + 60 * minute + 3600 * (Dates.adjusthour(hour, ampm)) + 86400 * Dates.totaldays(year, month, day))))
+        x = NanoDate(xdatetime, xnanosecs)
+        code |= OK
+    end
+    if eof(source, pos, len)
+        code |= EOF
+    end
+    return x, code, pos
+end
+
 
 #=
 Parsers.xparse(::Type{NanoDate}, source::Union{AbstractVector{UInt8},IO}, pos, len, options::Parsers.Options=Parsers.XOPTIONS, ::Type{S}=NanoDate) where {S} = true

e10f7c5808838a69b2803473f4f200d9e5256978
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-06-26 07:50:24 -0400

    up

--- test/devtest.jl
+++ test/devtest.jl
@@ -1,13 +1,23 @@
 using Dates, NanoDates
 import Parsers as P
 
-nd = NanoDate(2022, 6, 18,  12, 15, 30,  123, 456, 789);
-ndstr = string(nd); ndstrlen = length(ndstr);
+nd = NanoDate(2022, 6, 18, 12, 15, 30, 123, 456, 789);
+ndstr = string(nd);
+ndstrlen = length(ndstr);
 
-dfmt(::Type{NanoDate}) = "::Type{NanoDate}"
-dfmt(nd::NanoDate) = "nd::NanoDate"
-dfmt(x) = "unrecognized"
+dfsec = dateformat"yyyy-mm-ddTHH:MM:SS";
+dfmsec = dateformat"yyyy-mm-ddTHH:MM:SS.s";
+dfsss = dateformat"yyyy-mm-ddTHH:MM:SS.sss";
+dfn = dateformat"yyyy-mm-ddTHH:MM:SS.n";
+dfnnn = dateformat"yyyy-mm-ddTHH:MM:SS.nnn";
 
+posec = P.Options(dateformat="yyyy-mm-ddTHH:MM:SS");
+pomsec= P.Options(dateformat="yyyy-mm-ddTHH:MM:SS.s");
+posss = P.Options(dateformat="yyyy-mm-ddTHH:MM:SS.sss");
+pon   = P.Options(dateformat="yyyy-mm-ddTHH:MM:SS.n");
+ponnn = P.Options(dateformat="yyyy-mm-ddTHH:MM:SS.nnn");
+
+#=
 po1s = P.Options(stripwhitespace=true, stripquoted=true, dateformat="yyyy-mm-ddTHH:MM:SS.s");
 po2s = P.Options(stripwhitespace=true, stripquoted=true, dateformat="yyyy-mm-ddTHH:MM:SS.ss");
 po3s = P.Options(stripwhitespace=true, stripquoted=true, dateformat="yyyy-mm-ddTHH:MM:SS.sss");
@@ -27,5 +37,5 @@ println((tryparsed(ndstr), ndstr))
 
 tryxparsed(ndstr::AbstractString) = xparse(NanoDate, ndstr);
 println((tryxparsed(ndstr), ndstr))
-
+=#
 

77e1c4b3ee0a7ab0cac2f2466d610b2ccc9f2e8a
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-07-08 02:07:19 -0400

    remove dateformat

--- src/dateformat.jl
+++ src/dateformat.jl
@@ -1,3 +1,4 @@
+
 # returns the specifier part as a string
 Base.String(df::DateFormat) = string(df)[12:end-1]
 
@@ -180,89 +181,4 @@ function findandexpand(nd::NanoDate, str::AbstractString)
     strs = map(ch -> isletter(ch) ? ch2str(nd, ch) : string(ch), chrs)
     join(strs)
 end
-   
-     
-#=     
-     char2padfn[ch](value(char2period[ch](nd)), char2strlen[ch], '0') : ch, chrs)
-    (2022, ' ', 6, ' ', 18, ' ', 12, ' ', 15, ' ', 30, ' ', 123, 123, 123, 789, '-', 456)
-        julia > map(ch -> isletter(ch) ? char2period[ch] : ch, chrs)
-        (Year, ' ', Month, ' ', Day, ' ', Hour, ' ', Minute, ' ', Second, ' ', Millisecond, Millisecond, Millisecond, Nanosecond, '-', Microsecond)
 
-        julia > map(ch -> isletter(ch) ? char2period[ch](nd) : ch, chrs)
-        (Year(2022), ' ', Month(6), ' ', Day(18), ' ', Hour(12), ' ', Minute(15), ' ', Second(30), ' ', Millisecond(123), Millisecond(123), Millisecond(123), Nanosecond(789), '-', Microsecond(456))
-
-        julia > map(ch -> isletter(ch) ? value(char2period[ch](nd)) : ch, chrs)
-        (2022, ' ', 6, ' ', 18, ' ', 12, ' ', 15, ' ', 30, ' ', 123, 123, 123, 789, '-', 456)
-        fieldchars = unique(str[letters])
-
-        fieldinfo = [findfield(str, ch) for ch in fieldchars]
-        sort!(fieldinfo, lt=(x, y) -> (x.offset < y.offset))
-    =#
-    
-
-
-
-#=
-# regular expressions for matching dates, times, dates-with-times
-
-"""
-    Regular Expressions
-
-MiniMatchers
-
-  match without remembering
-  
-    *sep* in `date<sep>time` where sep ∈ ('T', ' ', ',')
-    
-        const MatchDateTimeSep = r"(?:(\s|T|,))"
-
-    *sep* in `year<sep>month`
-
-        const MatchYearMonthSep = r"(?:(\s|-|,))"
-
-    *sep* in `hour<sep>minute`
-
-        const MatchHourMinuteSep = r"(?:(\s|:|-|,|.))"
-
-    *sep* in `second<sep>millisecond`
-
-        const MatchSecondMillisSep = r"(?:(\s|.))"
-
-    *sep* in `millisecond<sep>microsecond`
-
-        const MatchMillisMicrosSep = r"(?:(\s|_|◦|,))"
-
-        "2022-05-04T07:30:45",  "2022-05-04 07:30:45"
-    separate_date_from_time = r"(\s|T)(?=\d)"
-    Match the character that separates the date-part from the time-part
-        where the separator character may be 'T' or may be ' ' (space)
-        
-    separator bto match either 'T' or ' '
-ISO8061_ymdhms matches
-
-    "2022-05-04T07:30:45",  "2022-05-04 07:30:45"
-    "2022-05-04T07:30:45",  "2022-05-04 07:30:45"
-
-=#
-
-#=
-regular_expressions
-
-# match without remembering
-  
-const MatchDateTimeSep = r"(?:(\s|T|^|,))"
-const MatchYearMonthSep = r"(?:(\s|-|,))"
-const MatchHourMinuteSep = r"(?:(\s|:|-|,|.))"
-const MatchSecondMillisSep = r"(?:(\s|.))"
-const MatchMillisMicrosSep = r"(?:(\s|_|◦|,))"
-
-# match to name
-
-const MatchYear = r"?<year>(\d{4}))"
-const MatchYMD = r"((?<year>(\d{4}))(?:(\s|-))(?<month>(\d{1,2}))(?:(\s|-))(?<day>(\d{1,2})))"
-const MatchHMS = r"((?<hour>(\d{4}))(?:(\s|:|-))(?<minute>(\d{1,2}))(?:(\s|:|-))(?<second>(\d{1,2})))"
-const MatchSubsecs = r"((?<millis>(\d{0,3}))(?:(_|' '))(?<micros>(\d{0,3}))(?:(_|' '))(?<nanos>(\d{0,3})))"
-
-const ISO8061_ymd_HMS = r"\d{4}(.\d{2}){2}(\s|T)(\d{2}.){2}\d{2}"
-
-=#

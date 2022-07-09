d1227ee87750b213e982cd510b15bb18c0616c03
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-07-09 00:54:32 -0400

    test timestamps

--- test/timestamp.jl
+++ test/timestamp.jl
@@ -1,9 +1,20 @@
 @testset "timestamp generation" begin
-
+    @test timestamp(nd; sep="_") == "2022-07-28T10:20:08.350_789_420Z"
+    @test timestamp(ndd; sep="_", postfix=false) == "2022-07-28T10:20:08.350_789_420"
 end
 
 @testset "timestamp parsing" begin
+    @test NanoDate("2022-06-18T12:15:30.123456789", dateformat"yyyy-mm-ddTHH:MM:SS.sssssssss") == NanoDate(2022, 06, 18, 12, 15, 30, 123, 456, 789)
+    @test NanoDate("2022-06-18T12:15:30.123456789Z", dateformat"yyyy-mm-ddTHH:MM:SS.sssssssssZ") == NanoDate(2022, 06, 18, 12, 15, 30, 123, 456, 789)
+    @test NanoDate("2022-06-18T12:15:30.123456789-04:00", dateformat"yyyy-mm-ddTHH:MM:SS.sssssssss+hh:mm"; localtime=true) == NanoDate(2022, 06, 18, 8, 15, 30, 123, 456, 789)
 
+    @test NanoDate("2022-06-18T12:15:30.123456", dateformat"yyyy-mm-ddTHH:MM:SS.ssssss") == NanoDate(2022, 06, 18, 12, 15, 30, 123, 456, 0)
+    @test NanoDate("2022-06-18T12:15:30.123456Z", dateformat"yyyy-mm-ddTHH:MM:SS.ssssssZ") == NanoDate(2022, 06, 18, 12, 15, 30, 123, 456, 0)
+    @test NanoDate("2022-06-18T12:15:30.123456-04:00", dateformat"yyyy-mm-ddTHH:MM:SS.ssssss+hh:mm"; localtime=true) == NanoDate(2022, 06, 18, 8, 15, 30, 123, 456, 0)
+
+    @test NanoDate("2022-06-18T12:15:30.12345", dateformat"yyyy-mm-ddTHH:MM:SS.ssss") == NanoDate(2022, 06, 18, 12, 15, 30, 123, 450, 0)
+    @test NanoDate("2022-06-18T12:15:30.12345Z", dateformat"yyyy-mm-ddTHH:MM:SS.ssssZ") == NanoDate(2022, 06, 18, 12, 15, 30, 123, 450, 0)
+    @test NanoDate("2022-06-18T12:15:30.12345-04:00", dateformat"yyyy-mm-ddTHH:MM:SS.ssss+hh:mm"; localtime=true) == NanoDate(2022, 06, 18, 8, 15, 30, 123, 450, 0)
 end
 
 @testset "ndtoday" begin

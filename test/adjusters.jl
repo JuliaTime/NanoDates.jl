198a3f016113ca453a6112148f566768a346f710
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-07-09 00:39:45 -0400

    test adjusters

--- test/adjusters.jl
+++ test/adjusters.jl
@@ -1,15 +1,15 @@
-@testset "adjusters .. truncation" begin
+@testset "adjusters .. @test truncation" begin
 
-   trunc(nd, Year) == firstdayofyear(nd)
-   trunc(nd, Quarter) == firstdayofquarter(nd)
-   trunc(nd, Month) == firstdayofmonth(nd)
-   trunc(nd, Week) == firstdayofweek(nd)
-   trunc(nd, Day) == firsthourofday(nd)
-   trunc(nd, Hour) == firstminuteofhour(nd)
-   trunc(nd, Minute) == firstsecondofminute(nd)
-   trunc(nd, Second) == firstmillisecondofsecond(nd)
-   trunc(nd, Millisecond) == firstmillisecondofsecond(nd)
-   trunc(nd, Microsecond) == firstmicrosecondofmillisecond(nd)
-   trunc(nd, Nanosecond) == firstnanosecondofmicrosecond(nd)
+   @test trunc(nd, Year) == firstdayofyear(nd)
+   @test trunc(nd, Quarter) == firstdayofquarter(nd)
+   @test trunc(nd, Month) == firstdayofmonth(nd)
+   @test trunc(nd, Week) == firstdayofweek(nd)
+   @test trunc(nd, Day) == firsthourofday(nd)
+   @test trunc(nd, Hour) == firstminuteofhour(nd)
+   @test trunc(nd, Minute) == firstsecondofminute(nd)
+   @test trunc(nd, Second) == firstmillisecondofsecond(nd)
+   @test trunc(nd, Millisecond) == firstmillisecondofsecond(nd)
+   @test trunc(nd, Microsecond) == firstmicrosecondofmillisecond(nd)
+   @test trunc(nd, Nanosecond) == firstnanosecondofmicrosecond(nd)
    
 end

ccf6552b95fc252cc72aec97c67bfee3b60dd0b9
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-07-13 15:24:54 -0400

    update io

--- src/io.jl
+++ src/io.jl
@@ -9,18 +9,8 @@ function Base.show(io::IO, nd::NanoDate,
     print(io, str)
 end
 
-
-Base.show(io::IO, nd::NanoDate) = 
-    Base.show(io::IO, m::MIME"text/plain", nd::NanoDate)
-
 Base.show(io::IO, m::MIME"text/plain", nd::NanoDate) =
     print(io, string(nd))
 
-Base.show(io::IO, nd::NanoDate, sep::Tuple{Vararg{Char}}) = 
-    Base.show(io::IO, m::MIME"text/plain", nd::NanoDate, sep)
-
-# to show values in the REPL and when using IJulia
-Base.show(io::IO, m::MIME"text/plain", 
-          nd::NanoDate, sep::Tuple{Vararg{Char}}) =
-    print(io, string(nd, sep))
-
+Base.show(io::IO, m::MIME"text/plain", nd::NanoDate, sep::Char) = 
+    print(io, string(nd; sep))

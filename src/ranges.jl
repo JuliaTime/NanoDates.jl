6e5802aaa8a23ced8e01855873e88ed1d77ec442
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-08-22 00:14:48 -0400

    Update ranges.jl

--- src/ranges.jl
+++ src/ranges.jl
@@ -1,8 +1,29 @@
+Base.zero(cperiod::Dates.CompoundPeriod) = Day(0) + Hour(0)
+Base.sign(cperiod::Dates.CompoundPeriod) =
+    if isempty(cperiod)
+       0
+    else
+      sign((canonical(cperiod)).periods[1])
+    end
+
+Base.abs(cperiod::Dates.CompoundPeriod) =
+    if sign(cperiod) === -1
+       -cperiod
+    else
+       cperiod
+    end
+
+
+Dates.value(cperiod::Dates.CompoundPeriod) =
+    sum(map(tons, cperiod.periods))
 
 for P in (:Year, :Month, :Day, :Hour, :Minute, :Second, :Millisecond, :Microsecond, :Nanosecond)
   steprange = StepRange{NanoDate, P}
   @eval begin
-     
+    function nanosteprange(nd1::NanoDate, p::Dates.Period, nd2::NanoDate)
+        separation = sign(p) * (nd1 - nd2)
+        periodsep = trunc(separation, typeof(p))
+        StepRange(nd1, periodsep, nd2)        
   end
 end
 

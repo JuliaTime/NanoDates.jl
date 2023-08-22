79e40d87cc096865b8c08f88d30cd197335dea40
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-08-22 10:31:44 -0400

    Update ranges.jl

--- src/ranges.jl
+++ src/ranges.jl
@@ -65,5 +65,5 @@ end
 
 Base.:(:)(a::NanoDate, b::NanoDate) = (:)(a, Day(1), b)
 
-guess(a::NanoDate, b::NanoDate, c) = floor(Int64, (Int128(value(b)) - Int128(value(a))) / toms(c))
+guess(a::NanoDate, b::NanoDate, c) = floor(Int64, (Int128(value(b)) - Int128(value(a))) / tons(c))
 len(a::NanoDate, b::NanoDate, c) = Int64(div(value(b - a), tons(c)))

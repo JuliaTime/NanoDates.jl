74069fd29ef999d8f6f6c795e20ae3f5e85b55c5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-08-21 23:38:27 -0400

    Update ranges.jl

--- src/ranges.jl
+++ src/ranges.jl
@@ -1,4 +1,12 @@
-Base.:(:)(a::T, b::T) where {T<:NanoDate} = (:)(a, Day(1), b)
+
+for P in (:Year, :Month, :Day, :Hour, :Minute, :Second, :Millisecond, :Microsecond, :Nanosecond)
+  steprange = StepRange{NanoDate, P}
+  @eval begin
+     
+  end
+end
+
+Base.:(:)(a::NanoDate, b::NanoDate) = (:)(a, Day(1), b)
 
 guess(a::NanoDate, b::NanoDate, c) = floor(Int64, (Int128(value(b)) - Int128(value(a))) / toms(c))
 len(a::NanoDate, b::NanoDate, c) = Int64(div(value(b - a), tons(c)))
